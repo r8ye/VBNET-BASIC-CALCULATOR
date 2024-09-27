acd5806ab83cf1bf2ada82eab5ad44206da5e7ca
zxraye <zxraye@gmail.com>
2024-09-28 00:25:53 +1000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,305 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtRersult = New TextBox()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btnAdd = New Button()
+        btnSubtract = New Button()
+        btn6 = New Button()
+        btn5 = New Button()
+        btn4 = New Button()
+        btnMultiply = New Button()
+        btn3 = New Button()
+        btn2 = New Button()
+        btn1 = New Button()
+        btnDivide = New Button()
+        btnEqual = New Button()
+        btnDelete = New Button()
+        btn0 = New Button()
+        lblNumerator = New Label()
+        lblOperator = New Label()
+        SuspendLayout()
+        ' 
+        ' txtRersult
+        ' 
+        txtRersult.Font = New Font("Segoe UI Symbol", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtRersult.Location = New Point(11, 47)
+        txtRersult.MaximumSize = New Size(500, 500)
+        txtRersult.Multiline = True
+        txtRersult.Name = "txtRersult"
+        txtRersult.Size = New Size(335, 86)
+        txtRersult.TabIndex = 0
+        txtRersult.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' btn7
+        ' 
+        btn7.BackColor = SystemColors.ButtonFace
+        btn7.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn7.Location = New Point(17, 153)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(80, 80)
+        btn7.TabIndex = 1
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = False
+        ' 
+        ' btn8
+        ' 
+        btn8.BackColor = SystemColors.ButtonFace
+        btn8.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn8.Location = New Point(99, 153)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(80, 80)
+        btn8.TabIndex = 2
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = False
+        ' 
+        ' btn9
+        ' 
+        btn9.BackColor = SystemColors.ButtonFace
+        btn9.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn9.Location = New Point(182, 153)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(80, 80)
+        btn9.TabIndex = 3
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = False
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.BackColor = SystemColors.ButtonFace
+        btnAdd.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btnAdd.Location = New Point(265, 153)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(80, 80)
+        btnAdd.TabIndex = 4
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = False
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.BackColor = SystemColors.ButtonFace
+        btnSubtract.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btnSubtract.Location = New Point(265, 239)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(80, 80)
+        btnSubtract.TabIndex = 9
+        btnSubtract.Text = "-"
+        btnSubtract.UseVisualStyleBackColor = False
+        ' 
+        ' btn6
+        ' 
+        btn6.BackColor = SystemColors.ButtonFace
+        btn6.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn6.Location = New Point(182, 239)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(80, 80)
+        btn6.TabIndex = 8
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = False
+        ' 
+        ' btn5
+        ' 
+        btn5.BackColor = SystemColors.ButtonFace
+        btn5.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn5.Location = New Point(99, 239)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(80, 80)
+        btn5.TabIndex = 7
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = False
+        ' 
+        ' btn4
+        ' 
+        btn4.BackColor = SystemColors.ButtonFace
+        btn4.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn4.Location = New Point(17, 239)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(80, 80)
+        btn4.TabIndex = 6
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = False
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.BackColor = SystemColors.ButtonFace
+        btnMultiply.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btnMultiply.Location = New Point(265, 325)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(80, 80)
+        btnMultiply.TabIndex = 14
+        btnMultiply.Text = "x"
+        btnMultiply.UseVisualStyleBackColor = False
+        ' 
+        ' btn3
+        ' 
+        btn3.BackColor = SystemColors.ButtonFace
+        btn3.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn3.Location = New Point(182, 325)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(80, 80)
+        btn3.TabIndex = 13
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = False
+        ' 
+        ' btn2
+        ' 
+        btn2.BackColor = SystemColors.ButtonFace
+        btn2.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn2.Location = New Point(99, 325)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(80, 80)
+        btn2.TabIndex = 12
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = False
+        ' 
+        ' btn1
+        ' 
+        btn1.BackColor = SystemColors.ButtonFace
+        btn1.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn1.Location = New Point(17, 325)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(80, 80)
+        btn1.TabIndex = 11
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = False
+        ' 
+        ' btnDivide
+        ' 
+        btnDivide.BackColor = SystemColors.ButtonFace
+        btnDivide.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btnDivide.Location = New Point(265, 411)
+        btnDivide.Name = "btnDivide"
+        btnDivide.Size = New Size(80, 80)
+        btnDivide.TabIndex = 19
+        btnDivide.Text = "\"
+        btnDivide.UseVisualStyleBackColor = False
+        ' 
+        ' btnEqual
+        ' 
+        btnEqual.BackColor = SystemColors.ButtonFace
+        btnEqual.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btnEqual.Location = New Point(182, 411)
+        btnEqual.Name = "btnEqual"
+        btnEqual.Size = New Size(80, 80)
+        btnEqual.TabIndex = 18
+        btnEqual.Text = "="
+        btnEqual.UseVisualStyleBackColor = False
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.BackColor = SystemColors.ButtonFace
+        btnDelete.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btnDelete.Location = New Point(99, 411)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(80, 80)
+        btnDelete.TabIndex = 17
+        btnDelete.Text = "DEL"
+        btnDelete.UseVisualStyleBackColor = False
+        ' 
+        ' btn0
+        ' 
+        btn0.BackColor = SystemColors.ButtonFace
+        btn0.Font = New Font("Segoe UI Symbol", 15F, FontStyle.Bold)
+        btn0.Location = New Point(17, 411)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(80, 80)
+        btn0.TabIndex = 16
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = False
+        ' 
+        ' lblNumerator
+        ' 
+        lblNumerator.AutoSize = True
+        lblNumerator.BackColor = SystemColors.ScrollBar
+        lblNumerator.Font = New Font("Segoe UI Symbol", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblNumerator.Location = New Point(262, 24)
+        lblNumerator.Name = "lblNumerator"
+        lblNumerator.Size = New Size(0, 17)
+        lblNumerator.TabIndex = 20
+        lblNumerator.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' lblOperator
+        ' 
+        lblOperator.AutoSize = True
+        lblOperator.Font = New Font("Segoe UI Symbol", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblOperator.Location = New Point(328, 24)
+        lblOperator.Name = "lblOperator"
+        lblOperator.Size = New Size(0, 17)
+        lblOperator.TabIndex = 21
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Menu
+        ClientSize = New Size(358, 512)
+        Controls.Add(lblOperator)
+        Controls.Add(lblNumerator)
+        Controls.Add(btnDivide)
+        Controls.Add(btnEqual)
+        Controls.Add(btnDelete)
+        Controls.Add(btn0)
+        Controls.Add(btnMultiply)
+        Controls.Add(btn3)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Controls.Add(btnSubtract)
+        Controls.Add(btn6)
+        Controls.Add(btn5)
+        Controls.Add(btn4)
+        Controls.Add(btnAdd)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(txtRersult)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Basic Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtRersult As TextBox
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btnEqual As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents lblNumerator As Label
+    Friend WithEvents lblOperator As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txtRersult.Text = txtRersult.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txtRersult.Text = txtRersult.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txtRersult.Text = txtRersult.Text + "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        txtRersult.Text = txtRersult.Text + "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txtRersult.Text = txtRersult.Text + "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txtRersult.Text = txtRersult.Text + "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txtRersult.Text = txtRersult.Text + "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txtRersult.Text = txtRersult.Text + "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txtRersult.Text = txtRersult.Text + "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txtRersult.Text = txtRersult.Text + "0"
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        lblNumerator.Text = txtRersult.Text
+        lblOperator.Text = "+"
+        txtRersult.Text = ""
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        lblNumerator.Text = txtRersult.Text
+        lblOperator.Text = "-"
+        txtRersult.Text = ""
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        lblNumerator.Text = txtRersult.Text
+        lblOperator.Text = "*"
+        txtRersult.Text = ""
+    End Sub
+
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        lblNumerator.Text = txtRersult.Text
+        lblOperator.Text = "\"
+        txtRersult.Text = ""
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        txtRersult.Text = ""
+        lblNumerator.Text = ""
+        lblOperator.Text = ""
+    End Sub
+
+    Private Sub btnEqual_Click(sender As Object, e As EventArgs) Handles btnEqual.Click
+        If lblOperator.Text = "+" Then
+            txtRersult.Text = Val(lblNumerator.Text) + (txtRersult.Text)
+            lblNumerator.Text = ""
+            lblOperator.Text = "="
+        End If
+
+        If lblOperator.Text = "-" Then
+            txtRersult.Text = Val(lblNumerator.Text) - Val(txtRersult.Text)
+            lblNumerator.Text = ""
+            lblOperator.Text = "="
+        End If
+
+        If lblOperator.Text = "*" Then
+            txtRersult.Text = Val(lblNumerator.Text) * Val(txtRersult.Text)
+            lblNumerator.Text = ""
+            lblOperator.Text = "="
+        End If
+
+        If lblOperator.Text = "\" Then
+            txtRersult.Text = Val(lblNumerator.Text) \ Val(txtRersult.Text)
+            lblNumerator.Text = ""
+            lblOperator.Text = "="
+        End If
+
+
+    End Sub
+End Class
